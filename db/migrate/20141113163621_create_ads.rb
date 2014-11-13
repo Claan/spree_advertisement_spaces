2058c746c4f6e6e319914ac77283ea1aa7081c8e
Nuno Polonia <nuno@claan.com>
2014-11-13 17:15:36 +0000

    Create Ad class

---
+++ db/migrate/20141113163621_create_ads.rb
@@ -0,0 +1,18 @@
+class CreateAds < ActiveRecord::Migration
+  def change
+    create_table :ads do |t|
+      t.string :title
+      t.attachment :image
+      t.string :link
+      t.string :size
+      t.boolean :show_in_sidebar
+      t.integer :position_in_sidebar
+      t.boolean :show_in_homepage
+      t.integer :position_in_homepage
+      t.integer :taxon_id
+      t.integer :position_in_taxon
+      
+      t.timestamps
+    end
+  end
+end
