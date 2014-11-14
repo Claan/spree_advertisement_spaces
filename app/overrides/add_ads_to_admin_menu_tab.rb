c521a74a1a29e1fc69b978ba7624d69a2313f8c6
Nuno Polonia <nuno@claan.com>
2014-11-14 14:40:39 +0000

    Change icon on the admin ad menu

--- app/overrides/add_ads_to_admin_menu_tab.rb
+++ app/overrides/add_ads_to_admin_menu_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "ads_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:ads, label: 'Ads', url: spree.admin_ads_path, icon: 'file') %>",
+                     :text => "<%= tab(:ads, label: 'Ads', url: spree.admin_ads_path, icon: 'newspaper-o') %>",
                      :disabled => false)
