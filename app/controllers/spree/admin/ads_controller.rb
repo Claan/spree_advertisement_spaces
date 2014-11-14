d5f06c8bbe0b9ff8a4a4a5d487e1aab961cfc730
Nuno Polonia <nuno@claan.com>
2014-11-14 14:41:21 +0000

    Backend ad index page

--- app/controllers/spree/admin/ads_controller.rb
+++ app/controllers/spree/admin/ads_controller.rb
@@ -1,3 +1,15 @@
 class Spree::Admin::AdsController < Spree::Admin::ResourceController
-
+  def index
+    if params[:from].present?
+      if params[:from] == "homepage"
+        @ads = Spree::Ad.from_homepage.order("position_in_homepage ASC")
+      elsif params[:from] == "sidebar"
+        @ads = Spree::Ad.from_sidebar.order("position_in_sidebar ASC")
+      elsif params[:from] == "taxons"
+        @ads = Spree::Ad.from_taxon.order('position_in_taxon ASC').group_by(&:taxon)
+      end
+    else
+      @ads = Spree::Ad.all
+    end
+  end
 end
--- app/models/spree/ad.rb
+++ app/models/spree/ad.rb
@@ -11,4 +11,20 @@ class Spree::Ad < ActiveRecord::Base
      path: ":rails_root/public/assets/blocks/:id/:style/:basename.:extension"  
 
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+  scope :from_homepage, -> { where show_in_homepage: true }
+  scope :from_sidebar, -> { where show_in_sidebar: true }
+  scope :from_taxon, -> { where.not taxon_id: nil }
+
+  def position
+    if self.show_in_homepage?
+      position = position_in_homepage
+    elsif self.show_in_sidebar?
+      position = position_in_sidebar
+    elsif self.taxon_id.present?
+      position = position_in_taxon
+    end 
+
+    position
+  end
 end
